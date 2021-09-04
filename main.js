1cdfe0c1e0e268a63c3017f103510eb0bc15a5aa
KindaExists <ehrencastillo@gmail.com>
2021-09-04 21:28:16 +0800

    add zoom

--- main.js
+++ main.js
@@ -14,13 +14,15 @@ const pencil = {
 
 // Canvas Setup
 const cv = {
+    group: document.querySelector('#canvas_group'),
+
     bg: document.querySelector('#bg_canvas'),
     main: document.querySelector('#main_canvas'),
     fg: document.querySelector('#fg_canvas'),
     debug: document.querySelector('#debug_canvas'),
 };
 
-const canvasRect = cv.main.getBoundingClientRect();
+let canvasRect = cv.main.getBoundingClientRect();
 
 
 const ctx = {
@@ -55,20 +57,20 @@ const mouse = {
     isHeld: false,
 
     updatePos(evt, zoom) {
-        this.x = evt.clientX;
-        this.y = evt.clientY;
-        this.canvas.x = (evt.clientX - canvasRect.left - zoom) / zoom;
-        this.canvas.y = (evt.clientY - canvasRect.top - zoom) / zoom;
+        this.x = evt.pageX;
+        this.y = evt.pageY;
+        this.canvas.x = (evt.pageX - canvasRect.left - zoom) / zoom;
+        this.canvas.y = (evt.pageY - canvasRect.top - zoom) / zoom;
     },
 };
 
 
-body.addEventListener('click', evt => {
+cv.group.addEventListener('click', evt => {
     mouse.updatePos(evt, zoom);
     canvasCtrl.drawPixel(ctx.main, [mouse.canvas.x, mouse.canvas.y], zoom)
 });
 
-body.addEventListener('mousemove', evt => {
+cv.group.addEventListener('mousemove', evt => {
     if (mouse.isHeld) {
         mouse.updatePos(evt, zoom);
         canvasCtrl.drawPixel(ctx.main, [mouse.canvas.x, mouse.canvas.y], zoom)
@@ -81,6 +83,11 @@ body.addEventListener('mousedown', evt => {
 
 body.addEventListener('mouseup', evt => {
     mouse.isHeld = false;
+});
+
+body.addEventListener('wheel', evt => {
+    const direction = (evt.wheelDelta > 0) ? 1 : -1;
+    canvasCtrl.changeZoom(cv.main, direction, [cv.main.width, cv.main.height]);
 });
 
 
@@ -99,8 +106,24 @@ const canvasCtrl = (() => {
         ctx.fillRect(pos[0], pos[1], pencil.size, pencil.size)
     }
 
+    function changeZoom(mainCanvas, direction, baseSize) {
+        if ((zoom < 16 && direction > 0) || (zoom > 1 && direction < 0)) {
+            if (direction > 0) {
+                zoom *= 2;
+            } else {
+                zoom /= 2;
+            }
+            const newWidth = baseSize[0] * zoom;
+            const newHeight = baseSize[1] * zoom;
+            document.documentElement.style.setProperty('--canvas-width', `${newWidth}px`);
+            document.documentElement.style.setProperty('--canvas-height', `${newHeight}px`);
+
+            canvasRect = mainCanvas.getBoundingClientRect();
+        }
+    }
+
     return {
-        drawPixel
+        drawPixel, changeZoom
     };
 })();
 
