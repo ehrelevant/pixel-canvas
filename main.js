29237f13fef4b50410a519c3e77d5852ae93fe16
KindaExists <ehrencastillo@gmail.com>
2021-09-04 19:37:07 +0800

    refactor code into modules/objects

--- main.js
+++ main.js
@@ -7,60 +7,86 @@ let zoom = 8;
 
 const body = document.body;
 
-const mainCanvas = document.querySelector('#main_canvas');
-const mainCtx = mainCanvas.getContext("2d");
+const cv = {
+    bg: document.querySelector('#bg_canvas'),
+    main: document.querySelector('#main_canvas'),
+    fg: document.querySelector('#fg_canvas'),
+    debug: document.querySelector('#debug_canvas'),
+};
 
-const debugCanvas = document.querySelector('#debug_canvas');
-const debugCtx = debugCanvas.getContext("2d");
+const canvasRect = cv.main.getBoundingClientRect();
 
 
-let canvasRect = mainCtx.canvas.getBoundingClientRect();
+const ctx = {
+    bg: cv.bg.getContext("2d"),
+    main: cv.main.getContext("2d"),
+    fg: cv.fg.getContext("2d"),
+    debug: cv.debug.getContext("2d"),
+
+    disableAllSmoothing() {
+        this.main.imageSmoothingEnabled = false;
+        this.debug.imageSmoothingEnabled = false;
+    }
+};
+
+ctx.disableAllSmoothing();
 
 
-if (mainCtx === null) {
+if (ctx.main === null) {
     alert("Unable to initialize Canvas. Your browser or machine may not support it.");
 }
 
-mainCtx.imageSmoothingEnabled = false;
-console.log(mainCtx)
-
 
 // Mouse Events/Functions
-let isMouseHeld = false;
+const mouse = {
+    x: undefined,
+    y: undefined,
+    canvas: {
+        x: undefined,
+        y: undefined,
+    },
+
+    isHeld: false,
+
+    updatePos(evt, zoom) {
+        this.x = evt.clientX;
+        this.y = evt.clientY;
+        this.canvas.x = (evt.clientX - canvasRect.left - zoom) / zoom;
+        this.canvas.y = (evt.clientY - canvasRect.top - zoom) / zoom;
+    },
+}
+
 
 body.addEventListener('click', evt => {
-    drawPixel(mainCtx, evt, zoom);
-})
+    mouse.updatePos(evt, zoom);
+    canvasCtrl.drawPixel(ctx.main, [mouse.canvas.x, mouse.canvas.y], zoom)
+});
 
 body.addEventListener('mousemove', evt => {
-    if (isMouseHeld) {
-        drawPixel(mainCtx, evt, zoom);
+    if (mouse.isHeld) {
+        mouse.updatePos(evt, zoom);
+        canvasCtrl.drawPixel(ctx.main, [mouse.canvas.x, mouse.canvas.y], zoom)
     }
 });
 
 body.addEventListener('mousedown', evt => {
-    isMouseHeld = true;
+    mouse.isHeld = true;
 });
 
 body.addEventListener('mouseup', evt => {
-    isMouseHeld = false;
+    mouse.isHeld = false;
 });
-
-function getMousePos(ctx, evt, zoom) {
-    const mouseX = evt.clientX - canvasRect.left - zoom;
-    const mouseY = evt.clientY - canvasRect.top - zoom;
-    return {
-        x: mouseX, y: mouseY
-    };
-}
 
 
 // Canvas Functions
+const canvasCtrl = (() => {
+    // Review (floored) "Bresenham Line algorithm" to avoid point skipping
+    function drawPixel(ctx, pos, zoom) {
+        ctx.fillStyle = primaryColor;
+        ctx.fillRect(pos[0], pos[1], pixelWidth, pixelWidth)
+    }
 
-// Review (floored) "Bresenham Line algorithm" to avoid point skipping
-function drawPixel(ctx, evt, zoom) {
-    ctx.fillStyle = primaryColor;
-
-    const mouse = getMousePos(ctx, evt, zoom);
-    ctx.fillRect(mouse.x / zoom, mouse.y / zoom, pixelWidth, pixelWidth)
-}
+    return {
+        drawPixel
+    };
+})();
